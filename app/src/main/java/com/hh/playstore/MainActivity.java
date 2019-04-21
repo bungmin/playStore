0a4969b941a46362a9a178756cbd01d1d8076b91
babobungmin <babobungmin@naver.com>
2019-04-21 15:56:14 +0900

    111111111

--- app/src/main/java/com/hh/playstore/MainActivity.java
+++ app/src/main/java/com/hh/playstore/MainActivity.java
@@ -1,8 +1,12 @@
 package com.hh.playstore;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.hh.playstore.databinding.ActivityMainBinding;
 
@@ -30,8 +34,47 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new AppAdapter(MainActivity.this, appList);
         act.appRankList.setAdapter(mAdapter);
 
+        act.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"확인버튼을 눌렀습니다.", Toast.LENGTH_SHORT ).show();
 
 
+
+            }
+        });
+
+        act.appRankList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Toast.makeText(MainActivity.this,String.format("%d번 줄 클릭", i), Toast.LENGTH_SHORT ).show();
+
+                App clickedAppData  =appList.get(i);
+                Intent intent = new Intent(MainActivity.this, AppDetailActivity.class);
+                intent.putExtra("제목", clickedAppData.title);
+                intent.putExtra("회사", clickedAppData.companyName);
+
+                intent.putExtra("앱정보", clickedAppData);
+                startActivity(intent);
+            }
+        });
+
+        act.appRankList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Toast.makeText(MainActivity.this,String.format("%d번 줄 클릭222", i), Toast.LENGTH_SHORT ).show();
+
+                appList.remove(i);
+                mAdapter.notifyDataSetChanged();
+
+
+
+                return true;
+            }
+        });
 
 
 
