5731392bca5b28f0b7d36c1556c7426658f41bfa
babobungmin <babobungmin@naver.com>
2019-04-21 10:48:50 +0900

    111111111

--- app/src/main/java/datas/App.java
+++ app/src/main/java/datas/App.java
@@ -8,7 +8,7 @@ public class App {
     public int userRating;  // 평균 평점(별 몇개?)
     public int price;
     public boolean isMine;  // true, 설치된 항목, false, 가격 표시
-    
+
 
     public App(int rank, String title, String companyName, int userRating, int price, boolean isMine) {
         this.rank = rank;
